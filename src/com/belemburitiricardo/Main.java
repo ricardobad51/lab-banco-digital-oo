18b050fa1aac80de36205d6d8b950ac6c6011907
ricardobad51 <rbbelem@gmail.com>
2022-01-03 11:39:27 -0300

    Mudança dos nomes

--- src/com/belemburitiricardo/Main.java
+++ src/com/belemburitiricardo/Main.java
@@ -9,11 +9,11 @@ import com.belemburitiricardo.entity.Conta;
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Cliente ricardo = new Cliente();
+		ricardo.setNome("Ricardo");
 		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Conta cc = new ContaCorrente(ricardo);
+		Conta poupanca = new ContaPoupanca(ricardo);
 
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
